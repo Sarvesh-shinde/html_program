2ed1039c8be51531a1a172daa609e77bfb46ef45
SARVESH <sarveshshinde2152@gmail.com>
2025-01-21 11:43:28 +0530

    Day four react

---
+++ my-app1/src/components/Test1.jsx
@@ -0,0 +1,8 @@
+function App(){
+    return (
+      <div>
+       <h2>This is Test1.jsx, a functional component</h2>
+      </div>
+    )
+  }
+  export default App;
---
+++ my-app1/src/components/Test2.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Test2 extends Component{
+    render(){
+        return(
+            <div>
+                <h1>This is Test2 class component</h1>
+            </div>
+            
+        )
+    }
+} 
+
+export default Test2;
