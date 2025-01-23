df0b520571360dcb2cf9e806c95deb3b8d91acf9
SARVESH <sarveshshinde2152@gmail.com>
2025-01-23 09:57:28 +0530

    Day seven Backend

---
+++ test/index.js
@@ -0,0 +1,106 @@
+// console.log("Welcome to nodejs application");
+
+
+
+//operating system module
+
+// const os=require('os')
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.freemem());
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(os.cpus());
+
+
+
+// const path=require('path')
+// console.log(__dirname)
+// console.log(__dirname)
+// console.log(path.dirname(__dirname))
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+
+
+// const fs=require('fs')
+// fs.readFile('one.txt','utf-8',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log(data)
+//     }
+// }
+// )
+
+
+
+
+// const fs=require('fs')
+// fs.writeFile('one.txt','utf-8',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("File created and text added succesfully")
+//     }
+// }
+// )
+
+
+
+
+// const fs=require('fs')
+// console.log("This is my first statement in console")
+
+// fs.readFile('one.txt','utf-8',(err,result)=>{
+// console.log(result);
+// }
+// )
+
+// console.log("This is my last statement in console")
+
+// const http=require('http');
+
+// const PORT =7788;
+
+
+// const myServer=http.createServer((req,res)=>{
+//     res.write("Backend application running successfully");
+//     res.write("one more nodejs application statement");
+//     res.end();
+// })
+
+// myServer.listen(PORT,()=>{
+//     console.log(`my Server is running on ${PORT} number `);
+// })
+
+
+// let { Addition, Multiplication } = require('./Add');
+
+
+// console.log("Addition result is " + Addition(10, 20));
+// console.log("Multiplication result is " + Multiplication(10,20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
